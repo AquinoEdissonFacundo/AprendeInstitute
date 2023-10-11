6ae45f8bf556f13ad544b9577f1cd4281e2dbe13
EdissonFacundo <aquinoedissonfacundo@gmail.com>
2023-10-10 23:09:50 -0300

    importaciones

--- AprendeInstitute/src/componentes/Home/home.jsx
+++ AprendeInstitute/src/componentes/Home/home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavBar from '../NavBar/navBar';
 import Form from '../Form/form';
-import Body from '../body/body';
+import Body from '../Body/body';
 import Footer from '../Footer/footer';
 import Cards from '../cards/cards';
 
